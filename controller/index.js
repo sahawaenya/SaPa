feb59de479fd3b50690a6b010f0418ec72c840ef
Rewinda <rewindawulansari@gmail.com>
2022-08-17 14:32:34 +0700

    controller

--- controller/index.js
+++ controller/index.js
@@ -3,7 +3,29 @@ const { Disease, Profile, Symptomp, User } = require ('../models/index')
 const app = express()
 
 class Controller {
+    static home (req,res) {
+        res.send ('SaPa hi..')
+    }
 
+    static signin (req,res) {
+        res.send ('SaPa hi..')
+    }
+
+    static login (req,res) {
+        res.send ('SaPa hi..')
+    }
+
+    static profile (req,res) {
+        res.send ('SaPa hi..')
+    }
+
+    static tesSymptomp (req,res) {
+        res.send ('SaPa hi..')
+    }
+
+    static diseaseResult (req,res) {
+        res.send ('SaPa hi..')
+    }
 }
 
 module.exports = Controller
