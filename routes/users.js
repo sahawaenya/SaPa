62228677855dd9e465c6493b11029b35241a1ab1
unknown <saha.waenya@gmail.com>
2022-08-18 02:07:10 +0700

    add Nav

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,8 @@ const routes = require('express').Router()
 const Controller = require('../controllers/users')
 
 // sign up (username, passwords, role, fullname, email)
+routes.get('/', Controller.home)
+
 routes.get('/signup', Controller.getSignup)
 routes.post('/signup', Controller.postSignup)
 
